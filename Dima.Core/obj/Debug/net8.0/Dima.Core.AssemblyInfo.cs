f1591adab4e506bd1f89e1853b3db96f23d807b3
Alexandre Malcar Veiga <alexandre.malcar@gmail.com>
2024-07-30 08:46:20 -0300

    feat(requests/models): jsut added Requests and Models for Reports

--- Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
+++ Dima.Core/obj/Debug/net8.0/Dima.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7142d609d7727f56d35869817a0583f4c6604e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec97a91128e0cfb39102e5fb64b0f505367112f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
