3cf0893cc0f5f0fe39c33d5395025a3461e090c7
Alexandre Malcar Veiga <alexandre.malcar@gmail.com>
2024-07-30 08:45:12 -0300

    feat: added some sql views for using in project

--- Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7142d609d7727f56d35869817a0583f4c6604e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc0d06e556df25ee36d127012c40d66c86f3172")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
